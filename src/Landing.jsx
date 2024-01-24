e9b542ffb31ab5d4fa52d489d70b33e9cfb44cdc
Austin Johnson <austintjohnson92@gmail.com>
2024-01-23 19:05:31 -0800

    on click animation

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -6,13 +6,13 @@ const Landing = () => {
   const [textFadeIn, setTextFadeIn] = useState(false);
   const [bottomFadeIn, setBottomFadeIn] = useState(false);
   const [clicked, setClicked] = useState(false);
-
+  const [hideText, setHideText] = useState(false);
+  const enterRef = useRef(null);
   // Calculate the viewport height
   const viewportHeight = window.innerHeight;
   // Check if the user has scrolled past 100 viewport heights
   window.addEventListener("scroll", () => {
     const scrollTop = window.scrollY;
-    console.log("here:", scrollTop);
     if (scrollTop > 1800) {
       setBottomFadeIn(true);
     }
@@ -34,18 +34,46 @@ const Landing = () => {
     }
   }, [fadeIn]);
 
-  console.log("here clicked:", clicked);
+  const spline = useRef();
+
+  function onLoad(splineApp) {
+    // save the app in a ref for later use
+    spline.current = splineApp;
+  }
+
+  function triggerAnimation() {
+    spline.current.emitEvent("mouseDown", "ENTER");
+    setHideText(true)
+    const waitToTransition = setTimeout(() => {
+      setClicked(true);
+    }, 1000);
+    return () => clearTimeout(waitToTransition);
+  }
+
   return (
     <>
-      <div className="h-screen overflow-hidden">
-        <Spline
-          className={`${
-            fadeIn ? "opacity-100" : "opacity-0"
-          } transition-opacity duration-1000`}
-          scene="https://prod.spline.design/KeFTy9Lpu76oQQj4/scene.splinecode"
-        />
-      </div>
-      {/* <AdhypeMain /> */}
+      {!clicked ? (
+        <div className="h-screen overflow-hidden relative">
+          <Spline
+            id="splineID"
+            className={`${
+              fadeIn ? "opacity-100" : "opacity-0"
+            } transition-opacity duration-500 `}
+            scene="https://prod.spline.design/GC7oSYPpKQCkjpYD/scene.splinecode"
+            onLoad={onLoad}
+          />
+            {!hideText ? (  <span
+            onClick={triggerAnimation}
+            className="cursor-pointer h-6 w-[90px] text-[20px] text-[white] absolute left-[calc(50%-35px)] bottom-[100px]"
+          >
+            ENTER
+          </span>) : null}
+        
+        </div>
+      ) : (
+        <AdhypeMain />
+      )}
+
       {/* <div
         className={`${
           fadeIn ? "opacity-100" : "opacity-0"
